ade83adcd28ff0fece5a5efa6e119fd75e402fc6
diansigit <diansigit.p@gmail.com>
2017-07-15 14:29:57 +0700

    First Initiate
    Facebook Login Button and Account Kit Training with React Native

---
+++ components/CheckoutScreen.js
@@ -0,0 +1,232 @@
+import React, { Component } from 'react';
+import { Image, View, StyleSheet, Text, ToastAndroid } from 'react-native';
+import { Container, Content, Header, Left, Button, Icon, Body, Title } from 'native-base';
+import PosterModel from '../Model/PosterModel';
+import AccountKit, { LoginButton } from 'react-native-facebook-account-kit';
+
+export default class CheckoutScreen extends React.Component {
+
+  static propTypes = {
+    poster: React.PropTypes.instanceOf(PosterModel).isRequired,
+  };
+
+  static defaultProps = {
+    poster: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      poster: this.props.poster,
+      authToken: null,
+      loggedAccount: null
+    };
+  }
+
+  componentWillMount = () => {
+    AccountKit.configure({
+      countryWhitelist: ["ID", "US", "CA"],
+      defaultCountry: "ID"
+    });
+    AccountKit.getCurrentAccessToken()
+    .then((token) => {
+      if (token) {
+        AccountKit.getCurrentAccount().then((account) => {
+          this.setState({
+            authToken: token,
+            loggedAccount: account
+          });
+          this.logUserPurchase();
+        })
+      } else {
+        console.log('No account logged in');
+      }
+    })
+    .catch((e) => console.log('Access token request failed', e));
+  };
+
+  render() {
+    return (
+      <Image
+        resizeMode="cover"
+        source={require('./../assets/images/login-splash-bg.jpg')}
+        style={styles.splashContainer}
+      >
+        <Container>
+          <Header style={{ backgroundColor: '#3770CC' }}>
+            <Left>
+              <Button transparent onPress={
+                () => {
+                  this.props.navigator.pop()
+                }
+              }>
+                <Icon name='md-arrow-round-back' />
+              </Button>
+            </Left>
+            <Body>
+              <Title style={{ fontSize: 15 }}>Checkout</Title>
+            </Body>
+          </Header>
+          <Content contentContainerStyle={styles.contentContainer}>
+            <View style={styles.imageTextWrapper}>
+              <View style={styles.congratsWrapper}>
+                <Text style={[styles.congrats]}>Thank You!</Text>
+              </View>
+              <View style={styles.imgWrapper}>
+                <Image
+                  resizeMode="contain"
+                  style={styles.img}
+                  source={{ uri: this.state.poster.thumbnailUri }}
+                />
+              </View>
+            </View>
+
+            { this.state.loggedAccount ? this.renderUserDetails() : this.renderLoginUI() }
+
+          </Content>
+        </Container>
+      </Image>
+    )
+  }
+
+  logUserPurchase() {
+    // assume that the "transaction" succeeded and the purchase was made
+    ToastAndroid.showWithGravity("Your purchase was successful", ToastAndroid.LONG, ToastAndroid.CENTER);
+  }
+
+  onLoginSuccess(token) {
+    if (!token) {
+      console.warn('User canceled login');
+      this.setState({});
+    } else {
+      AccountKit.getCurrentAccount().then((account) => {
+        this.setState({
+          authToken: token,
+          loggedAccount: account
+        })
+        console.log('user already logged in completing purchase');
+        this.logUserPurchase();
+      })
+    }
+  }
+
+  onLoginError(e) {
+    console.log('Failed to login', e);
+  }
+  
+  loginWithEmail() {
+    AccountKit.loginWithEmail().then((token) => {
+      this.onLoginSuccess(token);
+    }).catch((e) => {
+      this.onLoginError(e);
+    });
+  }
+
+  logout() {
+    AccountKit.logout().then(() => {
+      this.setState({
+        authToken: null,
+        loggedAccount: null
+      })
+    }).catch((e) => console.log('logout failed'));
+  }
+
+  renderLoginUI() {
+    return (
+      <View>
+        <Button
+          info
+          iconRight
+          block
+          rounded
+          style={{ margin: 10 }}
+          onPress={() => {
+            this.loginWithEmail()
+          }}
+        >
+          <Text style={[styles.btnText]}>Login with Email</Text>
+          <Icon name="md-mail" />
+        </Button>
+        <LoginButton
+          info
+          iconRight
+          block
+          rounded
+          style={{ margin: 10 }}
+          onPress={() => {
+            this.loginWithEmail()
+          }}
+        >
+          <Text style={[styles.btnText]}>Login with SMS</Text>
+          <Icon name="md-mail" />
+        </LoginButton>
+      </View>
+    );
+  }
+
+  renderUserDetails() {
+    const { id, email } = this.state.loggedAccount;
+    return (
+            <View>
+              <Text style={styles.akUser}>Account Kit User: {id}, {email}</Text>
+              <Button
+                info
+                iconRight
+                block
+                rounded
+                style={{ margin: 10 }}
+                onPress={() => {
+                  this.logout();
+                }}
+              >
+                <Text style={[styles.btnText]}>Logout</Text>
+              </Button>
+            </View>
+            );
+  }
+}
+
+const styles = StyleSheet.create({
+  splashContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  contentContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    padding: 10,
+  },
+
+  congratsWrapper: {
+    margin: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  congrats: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  imgWrapper: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  img: {
+    width: 300,
+    height: 300,
+  },
+  btnText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  akUser: {
+    fontSize: 15,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    margin: 10,
+    color: '#FFF',
+  }
+
+});
